b0a9e53c966807f923547643db1091c225be9c56
Luke Demi <lcd4910@g.rit.edu>
2024-04-04 09:38:01 -0400

    remove fgets

--- EchoClient/Source.c
+++ EchoClient/Source.c
@@ -49,8 +49,9 @@ int main(void) {
     // Main loop for sending messages
     while (1) {
         printf("Enter a message to send across the network (type 'Done' to exit): ");
-        fgets(userInput, 1024, stdin);
-        //scanf("%1025s", userInput);
+        //fgets(userInput, 1024, stdin);
+        scanf(" %[^\n]", userInput);
+        //printf("User Typed: %s\n", userInput); test code
 
         // Remove newline character
         userInput[strcspn(userInput, "\n")] = '\0';
